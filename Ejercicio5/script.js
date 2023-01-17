ee0efaa8b4c72aaaced5639b85da52b06db80a48
CarlosNavSeg <carlos.raco@hotmail.com>
2023-01-17 10:26:12 +0100

    xd

--- Ejercicio5/script.js
+++ Ejercicio5/script.js
@@ -1,6 +1,13 @@
-$('document').ready( function() {
-    $('#contentDeployer').click(
-        $.getJSON('//restcountries.eu/rest/v2/all', function(data) {
-            $('#contentSurface').load(JSON.parse(data))
-        })
-)})
+var url = "https://restcountries.com/v2/region/europe"
+$.ajax({
+    url: url,
+    method: 'GET',
+    }).done(function(result){
+    for(var i=0; i<result.length; i++){
+        $('#data').append(
+        ('<p>' + result[i].name + '</p>')
+        );
+    }
+    }).fail(function(err){
+    throw err;
+    });
